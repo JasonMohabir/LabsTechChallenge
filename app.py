6e360587b6d794c952662f930c7dc47559942fca
JasonMohabir <jm4590@columbia.edu>
2018-01-03 23:23:06 -0500

    Pushed finished

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route('/jm4590')
+def about_me():
+    return render_template('jm4590.html')
 
 if __name__ == '__main__':
     app.run()
